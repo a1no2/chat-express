44364e64e11a5e6b4dd480fc7290651aa8fc94ec
a1no2 <a1no2jz2l3@gmail.com>
2017-03-03 19:18:47 +0900

    DB追加、更新、削除ができる
    条件検索ができない

--- routes/login.js
+++ routes/login.js
@@ -5,36 +5,96 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'login' });
+	res.render('login', { title: 'login' });
 });
 
-module.exports = router;
+var MongoClient = require('mongodb').MongoClient;
+var collection;
 
 
 // セッション関連
 router.post('/', function(req, res, next) {
+	if (req.body.newUser == "新規登録") {
+		MongoClient.connect('mongodb://localhost:27017/chat',function(err ,db) {
+			// console.error(err);
+			console.log('open DB');
+
+			//mongoではテーブルではなくコレクション
+			collection = db.collection('user');
+
+			// 追加
+			var value = ({userName: req.body.userName  ,pass: req.body.pass});
+			collection.insertOne(value, function(err,r){
+				console.log("追加作業");
+			});
+			
+			// collection.remove();
+
+			collection.find().forEach(function(elem){
+				console.log(elem);
+
+			});
+
+			//DBとじ
+			db.on('clause', function(){
+				console.log('clause DB');
+			})
+			db.close();
+
+		});
 
 
-  if (req.body.newUser == "新規登録") {
-    req.session.user = {name: req.body.userName, pass: req.body.pass, login: true};
-  } else if (req.body.newUser == "ログイン") {
 
-  }
-    // console.log(req);
-    console.log(req.body.newUser);
-    console.log(req.session.user);
-    // console.log(req.session.user[name]);
+		// req.session.user = {login: 'true'};
 
-  
-  // if(req.body.userName) {
-  //   req.session.user = {name: req.body.userName};
-  //   req.session.pass = {name: req.body.pass};
-  //   res.redirect('./chat');
-  // } else {
-  //   var err = '入力が正しくありません。確認して再入力してください。';
-  //   res.render('login', {error: err});
-  // }
-  
+	} else if (req.body.login == "ログイン") {
+
+		MongoClient.connect('mongodb://localhost:27017/chat',function(err ,db) {
+			collection = db.collection('user');
+			// var user = collection.find({userName: req.body.userName, pass: req.body.pass});
+			// var user = collection.find();
+			// var user = collection.find({userName: req.body.userName});
+			// var naoki = db.user.find({userName: 'なおき'});
+			// console.log(req.body.userName);
+			// console.log(naoki + "");
+			
+			console.log(collection.find({"userName": req.body.userName},{"pass":true}));
+			// console.log(collection.find({"userName": req.body.userName},{}));
+			
+
+			//データ削除　アップデートと同じでわんとめにー
+			// collection.find(
+			// 	{userName: req.body.userName, pass: req.body.pass},
+			// 	function(err, r){
+			// 		console.log(r + "\n\n");
+			// 		console.log("err: " + err);
+			// 	}
+			// );
+			
+		});
+
+
+
+
+	}
+	// console.log(req);
+	// console.log(req.body.newUser);
+	// console.log(req.session.user);
+	// console.log(req.session.user.name);
+	// console.log(req.session.user[name]);
+
+
+	// if(req.body.userName) {
+	// req.session.user = {name: req.body.userName};
+	// req.session.pass = {name: req.body.pass};
+	// res.redirect('./chat');
+	// } else {
+	// var err = '入力が正しくありません。確認して再入力してください。';
+	// res.render('login', {error: err});
+	// }
+
 });
 
 
+
+module.exports = router;
